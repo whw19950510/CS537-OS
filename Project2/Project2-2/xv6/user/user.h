63a125e75e343c3f2c351b129dc26036b2a5ce0c
unknown <>
2017-10-04 23:11:02 -0500

    register syscall in user side

--- Project2/Project2-2/xv6/user/user.h
+++ Project2/Project2-2/xv6/user/user.h
@@ -25,7 +25,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-
+int getpinfo(struct pstat*);
 // user library functions (ulib.c)
 int stat(char*, struct stat*);
 char* strcpy(char*, char*);
